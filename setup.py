0fda72fd73426cae4e3c99c6500885cbdddc71cd
tfuxu <73042332+tfuxu@users.noreply.github.com>
2023-08-23 22:22:59 +0200

    fix: looks like only on manylinux image you can't exec pip as module

--- setup.py
+++ setup.py
@@ -17,10 +17,12 @@ if sys.platform == 'darwin':
     PYTHON_BINARY = os.getenv("PYTHON_BINARY_PATH", sys.executable)
     if PYTHON_BINARY == sys.executable:
         subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pybindgen'])
-else:
-    # linux & windows
+elif sys.platform == 'linux':
     PYTHON_BINARY = sys.executable
     subprocess.check_call(['pip', 'install', 'pybindgen'])
+else:
+    PYTHON_BINARY = sys.executable
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pybindgen'])
 
 
 def _generate_path_with_gopath() -> str:
