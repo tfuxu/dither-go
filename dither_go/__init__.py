44abb6b854c9c595b3181d8096c7f6e55eb109d1
tfuxu <73042332+tfuxu@users.noreply.github.com>
2023-09-04 23:28:50 +0200

    chore: update release information

--- dither_go/__init__.py
+++ dither_go/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2023, tfuxu <https://github.com/tfuxu>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from .wrapper import *
 from .matrices import *
